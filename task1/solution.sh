d065818a5434000ff8668fe35cb6d22ea88e76e9
Alexa Kamsky <146553204+Alexa-Kamsky@users.noreply.github.com>
2025-12-07 11:22:59 +0300

    Update solution.sh

---
+++ task1/solution.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Использовано: S0 <директория> <искать> <замена>"
+  exit 1
+fi
+
+DIR=$1
+SEARCH=$2
+REPLACE=$3
+
+find "$DIR" -type f -name "*.txt" -exec sed -i "s|${SEARCH}|${REPLACE}|g" {} \; 
+echo "Замена прошла успешно"
