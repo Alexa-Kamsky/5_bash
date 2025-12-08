7a34c47a38aa2a3e7879e231fd347961033ea597
Alexa <blackwupuwppienstein@gmail.com>
2025-12-08 20:26:46 +0300

    upd task8

--- task8/solution.sh
+++ task8/solution.sh
@@ -11,6 +11,6 @@ DIR_ARCHIVE=$2
 mkdir -p "$DIR_ARCHIVE"
 
 find "$DIR_LOG" -type f -name "*.log" -mtime +7 | while read file; do
-  base=$(basename "file")
-  gzip -c "file" > "$DIR_ARCHIVE/$base.gz"
+  base=$(basename $file)
+  gzip -c "$file" > "$DIR_ARCHIVE/$base.gz"
 done
